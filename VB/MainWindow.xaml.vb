0847ea817abd7ae6468dc20f94167ea5eec18ebe
DevExpressExampleBot <fakeEmail>
2022-12-18 18:37:04 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,33 +1,31 @@
-﻿Imports System
 Imports System.Data
 Imports System.Windows
+
 ' ...
-
 Namespace WpfChartSeriesTemplateBinding
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			Dim dt As New DataTable("MyDataTable")
+    Public Partial Class MainWindow
+        Inherits Window
 
-			dt.Columns.Add("Series", GetType(String))
-			dt.Columns.Add("Argument", GetType(DateTime))
-			dt.Columns.Add("Value", GetType(Double))
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-			dt.Rows.Add(New Object() { "A", DateTime.Today, 103 })
-			dt.Rows.Add(New Object() { "B", DateTime.Today, 200 })
-			dt.Rows.Add(New Object() { "C", DateTime.Today, 446 })
-			dt.Rows.Add(New Object() { "A", DateTime.Today.AddDays(1), 788 })
-			dt.Rows.Add(New Object() { "B", DateTime.Today.AddDays(1), 787 })
-			dt.Rows.Add(New Object() { "C", DateTime.Today.AddDays(1), 452 })
-			dt.Rows.Add(New Object() { "A", DateTime.Today.AddDays(2), 152 })
-			dt.Rows.Add(New Object() { "B", DateTime.Today.AddDays(2), 565 })
-			dt.Rows.Add(New Object() { "C", DateTime.Today.AddDays(2), 612 })
-
-			chartControl1.DataSource = dt
-		End Sub
-	End Class
+        Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim dt As DataTable = New DataTable("MyDataTable")
+            dt.Columns.Add("Series", GetType(String))
+            dt.Columns.Add("Argument", GetType(Date))
+            dt.Columns.Add("Value", GetType(Double))
+            dt.Rows.Add(New Object() {"A", Date.Today, 103})
+            dt.Rows.Add(New Object() {"B", Date.Today, 200})
+            dt.Rows.Add(New Object() {"C", Date.Today, 446})
+            dt.Rows.Add(New Object() {"A", Date.Today.AddDays(1), 788})
+            dt.Rows.Add(New Object() {"B", Date.Today.AddDays(1), 787})
+            dt.Rows.Add(New Object() {"C", Date.Today.AddDays(1), 452})
+            dt.Rows.Add(New Object() {"A", Date.Today.AddDays(2), 152})
+            dt.Rows.Add(New Object() {"B", Date.Today.AddDays(2), 565})
+            dt.Rows.Add(New Object() {"C", Date.Today.AddDays(2), 612})
+            Me.chartControl1.DataSource = dt
+        End Sub
+    End Class
 End Namespace
